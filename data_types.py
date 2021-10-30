926b3d4e993e3a4d31535e6f385a0b52bd27f8f5
Zhdanov-Dmitrii <48201807+Zhdanov-Dmitrii@users.noreply.github.com>
2021-10-30 16:05:53 +0300

    add id

--- data_types.py
+++ data_types.py
@@ -1,28 +1,33 @@
 # class with info about each adience and serialization method for jsonify
 class AudienceInfo(object):
+    id = ""
     building = ""
     audience_type = ""
     number = ""
 
-    def __init__(self, _building, _audience_type, _number):
+    def __init__(self, _id, _building, _audience_type, _number):
+        self.id = _id
         self.building = _building
         self.audience_type = _audience_type
         self.number = _number
 
     def serialize(self):
         return {
+            "id": self.id,
             "building": self.building,
             "type": self.audience_type,
             "number": self.number
         }
 
 class EducatorInfo(object):
+    id = ""
     fio = ""
     faculty = ""
     position = ""
     degree = ""
 
-    def __init__(self, _fio, _faculty, _position, _degree):
+    def __init__(self, _id, _fio, _faculty, _position, _degree):
+        self.id = _id
         self.fio = _fio
         self.faculty = _faculty
         self.position = _position
@@ -30,6 +35,7 @@ class EducatorInfo(object):
 
     def serialize(self):
         return {
+            "id": self.id,
             "fio": self.fio,
             "faculty": self.faculty,
             "position": self.position,
