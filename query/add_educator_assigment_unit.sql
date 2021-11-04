afee8527515d01ce1bc9385299f91543454ca969
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-04 11:39:40 +0300

    Update add_educator_assigment_unit.sql

--- query/add_educator_assigment_unit.sql
+++ query/add_educator_assigment_unit.sql
@@ -1,11 +1,11 @@
 use local_db
 create table EducatorAssigmentUnit (
 	id uniqueidentifier not NULL,
-	event uniqueidentifier NULL,
+	educator_assigment uniqueidentifier NULL,
 	educator_employment uniqueidentifier NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (event) references Event (id),
-	FOREIGN KEY (educator_employment) references EducatorEmployment (id)
+	FOREIGN KEY (educator_employment) references EducatorEmployment (id),
 );
 INSERT into [local_db].[dbo].[EducatorAssigmentUnit]([id]) SELECT [timetable-live].[dbo].[EducatorAssignmentUnit].[Oid] from [timetable-live].[dbo].[EducatorAssignmentUnit];
-update [local_db].[dbo].[EducatorAssigmentUnit] set event = (select [timetable-live].[dbo].[EducatorAssignmentUnit].[EducatorAssignment] from [timetable-live].[dbo].[EducatorAssignmentUnit] where [local_db].[dbo].[EducatorAssigmentUnit].id = [timetable-live].[dbo].[EducatorAssignmentUnit].Oid)
+update [local_db].[dbo].[EducatorAssigmentUnit] set educator_assigment = (select [timetable-live].[dbo].[Event].[EducatorAssignment] from [timetable-live].[dbo].[EducatorAssignmentUnit], [timetable-live].[dbo].[Event], [timetable-live].[dbo].[EducatorAssignment] where [local_db].[dbo].[EducatorAssigmentUnit].id = [timetable-live].[dbo].[EducatorAssignmentUnit].Oid and [timetable-live].[dbo].[Event].[EducatorAssignment] = [timetable-live].[dbo].[EducatorAssignment].[Oid] and [timetable-live].[dbo].[EducatorAssignmentUnit].[EducatorAssignment] = [timetable-live].[dbo].[EducatorAssignment].[Oid]);
+update [local_db].[dbo].[EducatorAssigmentUnit] set educator_employment = (select [timetable-live].[dbo].[EducatorAssignmentUnit].[EducatorEmployment] from [timetable-live].[dbo].[EducatorAssignmentUnit] where [local_db].[dbo].[EducatorAssigmentUnit].id = [timetable-live].[dbo].[EducatorAssignmentUnit].Oid);
