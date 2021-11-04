04a7df26687e9d2708cedbb78cddc492a461cf29
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-04 16:49:02 +0300

    Delete add_educator_group.sql

--- query/add_educator_group.sql
+++
@@ -1,8 +0,0 @@
-use local_db
-create table EducatorGroup (
-	id uniqueidentifier not NULL,
-	name nvarchar(100) NULL,
-	PRIMARY KEY (id)
-);
-INSERT into [local_db].[dbo].[EducatorGroup]([id]) SELECT [timetable-live].[dbo].[EducatorEmploymentGroup].[Oid] from [timetable-live].[dbo].[EducatorEmploymentGroup];
-update [local_db].[dbo].[EducatorGroup] set name = (select [timetable-live].[dbo].[EducatorEmploymentGroup].[Name] from [timetable-live].[dbo].[EducatorEmploymentGroup] where [local_db].[dbo].[EducatorGroup].id = [timetable-live].[dbo].[EducatorEmploymentGroup].Oid);
