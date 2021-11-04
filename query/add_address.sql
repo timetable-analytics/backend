05eea5ebfec80cad224f365dd533b195c0b6dc6a
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-04 16:48:21 +0300

    Delete add_address.sql

--- query/add_address.sql
+++
@@ -1,8 +0,0 @@
-use local_db
-create table Address (
-	id uniqueidentifier not NULL,
-	address nvarchar(250) NULL,
-	PRIMARY KEY (id)
-);
-INSERT into [local_db].[dbo].[Address]([id]) SELECT [timetable-live].[dbo].[Address].[Oid] from [timetable-live].[dbo].[Address];
-update [local_db].[dbo].[Address] set address = (select [timetable-live].[dbo].[Address].[DisplayName1] from [timetable-live].[dbo].[Address] where [local_db].[dbo].[Address].id = [timetable-live].[dbo].[Address].Oid)
