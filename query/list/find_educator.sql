8d7f47fc44577e26f3e68d126efe5beff08ef49e
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-04 16:51:38 +0300

    Create find_educator.sql

---
+++ query/list/find_educator.sql
@@ -0,0 +1,15 @@
+use local_db
+select [Division].[name], [Educator].[full_name], [EducatorPosition].[name], [EducatorGroup].[name]
+from [Division], [Educator], [EducatorPosition], [EducatorGroup], [EducatorEmployment], [EducatorDivision]
+where [Division].[id] = [EducatorDivision].[division]
+and [Educator].[id] = [EducatorEmployment].[educator]
+and [EducatorPosition].[id] = [EducatorEmployment].[position]
+and [EducatorGroup].[id] = [EducatorEmployment].[educator_group]
+and [EducatorEmployment].[id] = [EducatorDivision].[educator_employment]
+and [EducatorEmployment].[is_active] = 1
+and [Division].[name] like '%%'
+and [Educator].[full_name] like '%%'
+and [EducatorPosition].[name] like '%%'
+and [EducatorGroup].[name] like '%%'
+group by [Division].[name], [Educator].[full_name], [EducatorPosition].[name], [EducatorGroup].[name] --так как упоминается несколько раз одна и та же должность
+order by [Educator].[full_name]
