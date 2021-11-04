191b1d45b85e3b3e1b5e1ec36339e9d2c68961f1
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-04 16:51:19 +0300

    Create find_classroom.sql

---
+++ query/list/find_classroom.sql
@@ -0,0 +1,8 @@
+use [local_db]
+select [Address].[address], [Classroom_kind].[name], [Classroom].[name]
+from [Address], [Classroom], [Classroom_kind]
+where [Classroom].[classroom_kind] = [Classroom_kind].[id]
+and [Classroom].[address] = [Address].[id]
+and [Address].[address] like '%%'
+and [Classroom].[name] like '%%'
+and [Classroom_kind].[name] like '%%'
