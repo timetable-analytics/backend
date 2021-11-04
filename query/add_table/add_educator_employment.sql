d74dac1edb8e8a2b6460cf17ea2143afedc96c69
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-04 16:50:22 +0300

    Create add_educator_employment.sql

---
+++ query/add_table/add_educator_employment.sql
@@ -0,0 +1,17 @@
+use local_db
+create table EducatorEmployment (
+	id uniqueidentifier not NULL,
+	position uniqueidentifier NULL,
+	educator uniqueidentifier NULL,
+	educator_group uniqueidentifier NULL,
+	is_active bit NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (position) references EducatorPosition (id),
+	FOREIGN KEY (educator) references Educator (id),
+	FOREIGN KEY (educator_group) references EducatorGroup (id)
+);
+INSERT into [local_db].[dbo].[EducatorEmployment]([id]) SELECT [timetable-live].[dbo].[EducatorEmployment].[Oid] from [timetable-live].[dbo].[EducatorEmployment];
+update [local_db].[dbo].[EducatorEmployment] set position = (select [timetable-live].[dbo].[EducatorEmployment].[Position] from [timetable-live].[dbo].[EducatorEmployment] where [local_db].[dbo].[EducatorEmployment].id = [timetable-live].[dbo].[EducatorEmployment].Oid);
+update [local_db].[dbo].[EducatorEmployment] set educator = (select [timetable-live].[dbo].[EducatorEmployment].[EducatorMasterPerson] from [timetable-live].[dbo].[EducatorEmployment] where [local_db].[dbo].[EducatorEmployment].id = [timetable-live].[dbo].[EducatorEmployment].Oid);
+update [local_db].[dbo].[EducatorEmployment] set educator_group = (select [timetable-live].[dbo].[EducatorEmployment].[Group] from [timetable-live].[dbo].[EducatorEmployment] where [local_db].[dbo].[EducatorEmployment].id = [timetable-live].[dbo].[EducatorEmployment].Oid);
+update [local_db].[dbo].[EducatorEmployment] set is_active = (select [timetable-live].[dbo].[EducatorEmployment].[IsActive] from [timetable-live].[dbo].[EducatorEmployment] where [local_db].[dbo].[EducatorEmployment].id = [timetable-live].[dbo].[EducatorEmployment].Oid);
