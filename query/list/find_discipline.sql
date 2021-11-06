3f04d999eb514933a1934c2482139da037db0169
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-06 19:02:16 +0300

    Update find_discipline.sql

--- query/list/find_discipline.sql
+++ query/list/find_discipline.sql
@@ -1,5 +1,4 @@
 use local_db
-select [Discipline].[name]
+select [Discipline].[id], [Discipline].[name]
 from [Discipline]
-group by [Discipline].[name] --because there are repetitions
 order by [Discipline].[name]
