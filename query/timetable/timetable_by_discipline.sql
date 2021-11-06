519b370d8eccc8827c4187a8b006aca12ca2a161
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-07 00:02:43 +0300

    Create timetable_by_discipline.sql

---
+++ query/timetable/timetable_by_discipline.sql
@@ -0,0 +1,27 @@
+use local_db
+select 	[Address].[address], 
+		[Classroom].[name], 
+		[Educator].[full_name], 
+		[Discipline].[name], 
+		[Group_union].[name], 
+		(convert(nvarchar, [Event].[start]) + ' - ' + ltrim(right(convert(varchar(20), [Event].[end], 100), 7)))
+from	[Address], 
+		[Classroom], 
+		[Educator], 
+		[Discipline], 
+		[Group_union], 
+		[Event], 
+		[EducatorEmployment],
+		[EducatorAssigmentUnit],
+		[EventLocation]
+where [Address].[id] = [Classroom].[address]
+and [Classroom].[id] = [EventLocation].[classroom]
+and [EventLocation].[event] = [Event].[id]
+and [EducatorEmployment].[educator] = [Educator].[id]
+and [EducatorEmployment].[id] = [EducatorAssigmentUnit].[educator_employment]
+and [Event].[educator_assigment] = [EducatorAssigmentUnit].[educator_assigment]
+and [Event].[discipline] = [Discipline].[id]
+and [Group_union].[id] = [Event].[group_union]
+and [Discipline].[id] in (/* list id in '' separated by , */)
+and convert(date, [Event].[start]) >= /*'yyy-mm-dd'*/
+and convert(date, [Event].[end]) <= /*'yyyy-mm-dd'*/
