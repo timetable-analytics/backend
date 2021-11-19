6210a0535f099a4be3f0dc1b52116040658498ff
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-19 13:58:38 +0300

    Create by_educator.sql

---
+++ query/for graph/by_educator.sql
@@ -0,0 +1,19 @@
+select count([Event].[id]), 
+	[Educator].[full_name],  
+	--/* day */ (convert(date, [Event].[start]))
+	--/* month */ (convert(varchar,year([Event].[start])) + '-' + convert(varchar,month([Event].[start])))
+	--/* year */ year([Event].[start])
+from [Event],
+	[Educator],
+	[EducatorAssigmentUnit],
+	[EducatorEmployment]
+where [Educator].[id] = [EducatorEmployment].[educator]
+and [EducatorEmployment].[id] = [EducatorAssigmentUnit].[educator_employment]
+and [EducatorAssigmentUnit].[educator_assigment] = [Event].[educator_assigment]
+and [Event].[id] in (/* list id in '' separated by , */)
+--/* for day */ group by (convert(date, [Event].[start])), [Educator].[full_name]
+--/* for month */ group by (convert(varchar,year([Event].[start])) + '-' + convert(varchar,month([Event].[start]))) , [Educator].[full_name] 
+--/* for year */ group by year([Event].[start]), [Educator].[full_name]
+--/* for day */ order by [Educator].[full_name], (convert(date, [Event].[start]))
+--/* for month */ order by [Educator].[full_name], (convert(varchar,year([Event].[start])) + '-' + convert(varchar,month([Event].[start])))
+--/* for year */ order by [Educator].[full_name], year([Event].[start])
