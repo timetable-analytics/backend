1b3c8628607724201d5aa80387503ce313a4fa50
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-21 20:33:14 +0300

    Update by_educator.sql

--- query/for graph/by_educator.sql
+++ query/for graph/by_educator.sql
@@ -10,7 +10,7 @@ from [Event],
 where [Educator].[id] = [EducatorEmployment].[educator]
 and [EducatorEmployment].[id] = [EducatorAssigmentUnit].[educator_employment]
 and [EducatorAssigmentUnit].[educator_assigment] = [Event].[educator_assigment]
-and [Event].[id] in (/* list id in '' separated by , */)
+and [Event].[id] in (/* list id in '' separated by , (for example '1', '2')*/)
 --/* for day */ group by (convert(date, [Event].[start])), [Educator].[full_name]
 --/* for month */ group by (convert(varchar,year([Event].[start])) + '-' + convert(varchar,month([Event].[start]))) , [Educator].[full_name] 
 --/* for year */ group by year([Event].[start]), [Educator].[full_name]
