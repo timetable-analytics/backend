100098671d520394b5ee85fcf4aff01a4583fbdb
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-21 20:37:45 +0300

    Update educator.sql

--- query/list_by_event/educator.sql
+++ query/list_by_event/educator.sql
@@ -7,6 +7,6 @@ and [EducatorGroup].[id] = [EducatorEmployment].[educator_group]
 and [EducatorEmployment].[id] = [EducatorDivision].[educator_employment]
 and [Event].[educator_assigment] = [EducatorAssigmentUnit].[educator_assigment]
 and [EducatorAssigmentUnit].[educator_employment] = [EducatorEmployment].[id]
-and [Event].[id] in (/* list id in '' separated by , */)
+and [Event].[id] in (/* list id in '' separated by , (for example '1', '2')*/)
 group by [Division].[name], [Educator].[full_name], [EducatorPosition].[name], [EducatorGroup].[name], [Educator].[id]
 order by [Educator].[full_name]
