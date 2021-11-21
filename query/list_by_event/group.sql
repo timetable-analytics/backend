a78d1fdc5813463c10c290261dac5637b6adb326
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-11-21 20:38:01 +0300

    Update group.sql

--- query/list_by_event/group.sql
+++ query/list_by_event/group.sql
@@ -3,6 +3,6 @@ from [Division], [Group_union], [Group_union_Division], [Event]
 where [Division].[id] = [Group_union_Division].[division]
 and [Group_union].[id] = [Group_union_Division].[group_union]
 and [Event].[group_union] = [Group_union].[id]
-and [Event].[id] in (/* list id in '' separated by , */)
+and [Event].[id] in (/* list id in '' separated by , (for example '1', '2')*/)
 group by [Group_union].[id], [Division].[name], [Group_union].[name], [Group_union].[course]
 order by [Group_union].[name]
