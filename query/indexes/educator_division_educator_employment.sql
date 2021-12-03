8c78c3d8ee471a20dbaadceb09b04228c817f178
Anastasia-Petrukhanova <61744893+Anastasia-Petrukhanova@users.noreply.github.com>
2021-12-03 20:29:18 +0300

    indexes

---
+++ query/indexes/educator_division_educator_employment.sql
@@ -0,0 +1,11 @@
+USE [local_db]
+GO
+
+/****** Object:  Index [NonClusteredIndex-20211202-202031]    Script Date: 03.12.2021 20:19:43 ******/
+CREATE NONCLUSTERED INDEX [NonClusteredIndex-20211202-202031] ON [dbo].[EducatorDivision]
+(
+	[educator_employment] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+
